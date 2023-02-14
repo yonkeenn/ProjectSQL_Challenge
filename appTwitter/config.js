751ce30b17299c103c4c42a8cda91606a4442da6
yonkeenn <jhon.vargasr@gmail.com>
2023-02-13 23:07:57 -0500

    Start

--- appTwitter/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    consumer_key:         'wjt04n7xmXFl0uEUs9HhDr36y',
-    consumer_secret:      'MYx7fwmqg5ZmjKEGey3eOycxCfaUQ6bS3Byte0GscST3hHY2XF',
-    access_token:         '2701401562-xYEQJqyLA4EjXjzsKpDa19z98akfAZGpGgW8AKn',
-    access_token_secret:  'yWMiUoj82l3yimOpUOpzo7sd88gc9JbzpBCdnLUTognMw',
-    Bearer_Token: 'AAAAAAAAAAAAAAAAAAAAALO5lgEAAAAAAsvBLqrmwTES6LVIUxrcjVSLKrQ%3DQgRgvx2BGEwjUOEApVvdpndYbWViKsEAGbzENV2FVCF3v745Qx'
-    //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    //strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  };
